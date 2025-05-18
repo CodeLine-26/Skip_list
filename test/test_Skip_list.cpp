f39bbb4c5e7294fac9b9ed12740c106c1cdf2b7d
Artur <arurwer@gmail.com>
2025-05-19 01:32:35 +0300

    -

--- test/test_Skip_list.cpp
+++ test/test_Skip_list.cpp
@@ -1,79 +1,81 @@
 ﻿#include <gtest.h>
 #include "Skip_list.h"
 
-class SkipListTesting : public ::testing::Test
-{
-protected:
-    void SetUp() override
+
+TEST(SkipListTest, InsertAndSearch) {
+    SkipList<int> list;
+    list.insert(3);
+    list.insert(6);
+    list.insert(2);
+    list.insert(9);
+    EXPECT_TRUE(list.search(3));
+    EXPECT_TRUE(list.search(6));
+    EXPECT_TRUE(list.search(2));
+    EXPECT_TRUE(list.search(9));
+    EXPECT_FALSE(list.search(1));
+    EXPECT_FALSE(list.search(10));
+}
+
+TEST(SkipListTest, Erase) {
+    SkipList<int> list;
+    list.insert(5);
+    list.insert(10);
+    list.insert(15);
+    list.erase(10);
+    EXPECT_FALSE(list.search(10));
+    EXPECT_TRUE(list.search(5));
+    EXPECT_TRUE(list.search(15));
+    list.erase(20);
+    EXPECT_EQ(list.size(), 2);
+}
+
+TEST(SkipListTest, SizeAndEmpty) {
+    SkipList<int> list;
+    EXPECT_TRUE(list.empty());
+    EXPECT_EQ(list.size(), 0);
+    list.insert(1);
+    list.insert(2);
+    EXPECT_EQ(list.size(), 2);
+    EXPECT_FALSE(list.empty());
+    list.erase(1);
+    EXPECT_EQ(list.size(), 1);
+    list.erase(2);
+    EXPECT_TRUE(list.empty());
+}
+
+TEST(SkipListTest, Duplicates) {
+    SkipList<int> list;
+    list.insert(5);
+    list.insert(5);  
+    EXPECT_EQ(list.size(), 1);  
+}
+
+TEST(SkipListTest, Print) {
+    SkipList<int> list;
+    list.insert(1);
+    list.insert(3);
+    list.insert(2);
+    EXPECT_NO_THROW(list.print());
+}
+
+TEST(SkipListTest, Clear) {
+    SkipList<int> list;
+
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.erase(10);
+    list.erase(20);
+    list.erase(30);
+    EXPECT_TRUE(list.empty());
+    EXPECT_EQ(list.size(), 0);
+}
+
+TEST(SkipListTest, MaxLevel) {
+    SkipList<int> list(5, 0.5); 
+    for (int i = 0; i < 100; ++i) 
     {
-        SkipList = new SkipList();
+        list.insert(i);
     }
-    void TearDown() override
-    {
-        delete SkipList;
-    }
-    SkipList* SkipList;
-};
-
-TEST_F(SkipListTesting, SingleInsert) 
-{
-    SkipList->insert(1);
-    EXPECT_TRUE(SkipList->search(1));
-    EXPECT_FALSE(SkipList->search(2));
-}
-
-TEST_F(SkipListTesting, EmptyList) 
-{
-    EXPECT_FALSE(SkipList->search(1));
-    EXPECT_FALSE(SkipList->erase(1));
-}
-
-TEST_F(SkipListTesting, DuplicateInserts) 
-{
-    SkipList->insert(1);
-    SkipList->insert(1);
-    SkipList->insert(1);
-
-    EXPECT_TRUE(SkipList->erase(1));
-    EXPECT_TRUE(SkipList->search(1));
-}
-
-TEST_F(SkipListTesting, MultipleInserts) 
-{
-    SkipList->insert(1);
-    SkipList->insert(2);
-    SkipList->insert(3);
-    SkipList->insert(4);
-
-    EXPECT_TRUE(SkipList->search(1));
-    EXPECT_TRUE(SkipList->search(2));
-    EXPECT_TRUE(SkipList->search(3));
-    EXPECT_TRUE(SkipList->search(4));
-    EXPECT_FALSE(SkipList->search(5));
-}
-
-TEST_F(SkipListTesting, EraseFromEmpty) 
-{
-    EXPECT_FALSE(SkipList->erase(1));
-}
-
-TEST_F(SkipListTesting, InsertNegativeNumbers)
-{
-    SkipList->insert(-1);
-    SkipList->insert(-2);
-
-    EXPECT_TRUE(SkipList->search(-1));
-    EXPECT_TRUE(SkipList->search(-2));
-    EXPECT_FALSE(SkipList->search(0));
-}
-
-TEST_F(SkipListTesting, PrintNonEmpty) 
-{
-    SkipList->insert(1);
-    SkipList->insert(2);
-    testing::internal::CaptureStdout();
-    SkipList->print();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_TRUE(output.find("1") != std::string::npos);
-    EXPECT_TRUE(output.find("2") != std::string::npos);
+    EXPECT_NO_THROW(list.print());
 }
